186f99ee4b5dc908f06234b5067a1ac5c2c8614d
unknown <>
2016-07-24 20:11:48 +0900

    asd

--- scripts/myangular.js
+++ scripts/myangular.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', [], function($interpolateProvider) {
+var app = angular.module('myApp', [], function($interpolateProvider) {
   $interpolateProvider.startSymbol('[[');
   $interpolateProvider.endSymbol(']]');
 });
