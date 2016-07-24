06c812204c366c972a860b3fd20c6b2156cd2b69
unknown <>
2016-07-24 21:31:58 +0900

    chat

--- scripts/myangular.js
+++ scripts/myangular.js
@@ -32,7 +32,7 @@ app.controller('appCtl',['$scope', '$window','socket',  function($scope, $window
 
 
 app.factory('socket', function ($rootScope) {
-  var socket = io.connect('https://ec2-54-199-240-31.ap-northeast-1.compute.amazonaws.com');
+  var socket = io.connect('https://ec2-54-199-240-31.ap-northeast-1.compute.amazonaws.com:443' , {secure: true});
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {
