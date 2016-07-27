dd1034f7bb9d1a943ac3b27e497dc5df1b004c61
unknown <>
2016-07-27 15:34:38 +0900

    asd

--- javascripts/myangular.js
+++ javascripts/myangular.js
@@ -64,7 +64,7 @@ app.controller('appCtl',['$scope', '$window','socket','$anchorScroll','$location
 
 
 app.factory('socket', function ($rootScope) {
-  var socket = io('https://ec2-54-199-240-31.ap-northeast-1.compute.amazonaws.com' , {secure: true});
+  var socket = io.connect('https://54.199.240.31' , {secure: true});
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {
